7c6bc6f81d79e64624335e700e0d1279b3c8cae4
Ameen Eetemadi <ameenetemady@github.com>
2021-06-29 22:23:59 -0700

    documented

--- convert_to_friendlyname_OMMs.R
+++ convert_to_friendlyname_OMMs.R
@@ -1,4 +1,6 @@
 #!/usr/bin/env Rscript
+#' Replace the uids in a mixed-meals file, with the corresponding friendly food names.
+
 suppressPackageStartupMessages(library("argparse"))
 library(stringr)
 
@@ -20,6 +22,11 @@ get_args <- function() {
 
 args <- get_args()
 
+# 1) Load mixed-meals
 MMs <- load_MMs(args$OMMs)
+
+# 2) Replace the column names (uids) with corresponding friendly names
 colnames(MMs) <- get_friendly_food_names(colnames(MMs), args$glycanDB)
+
+# 3) Save
 save_MMs(MMs, args$output_filename)
